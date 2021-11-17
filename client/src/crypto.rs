06e072d575b19187f43ef9feb5a5187cf1cdc2a9
Vincent <vincent.lagerros@gmail.com>
2021-11-18 00:36:34 +0100

    fixed password len

--- client/src/crypto.rs
+++ client/src/crypto.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use rand::Rng;
 use ring::error::Unspecified;
 
@@ -8,6 +10,13 @@ fn generate_random_nonce() -> [u8; 12] {
 
 // TODO KEY NOT PASSWORD?
 
+fn generate_key(password: String)-> Result<aes_gcm_siv::Key<aes_gcm_siv::aead::consts::U32>, String> {
+    let mut pass_padded: Vec<u8> = vec![0; 32];
+    pass_padded.as_mut_slice().write(password.as_bytes()).map_err(|e| format!("write error {}", e))?;
+
+    Ok(*aes_gcm_siv::Key::from_slice(&pass_padded))
+}
+
 /** password in plain text, returns decrypted message */
 pub fn decrypt_bytes(
     bytes: Vec<u8>,
@@ -15,11 +24,10 @@ pub fn decrypt_bytes(
     nonce_bytes: [u8; 12],
 ) -> Result<Vec<u8>, String> {
     use aes_gcm_siv::aead::{Aead, NewAead};
-    use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
+    use aes_gcm_siv::{Aes256GcmSiv, Nonce}; // Or `Aes128GcmSiv`
 
-    // TODO convert password to 256-bit
-    let key = Key::from_slice(password.as_bytes());
-    let cipher = Aes256GcmSiv::new(key);
+    let key = generate_key(password)?;
+    let cipher = Aes256GcmSiv::new(&key);
 
     let nonce = &Nonce::from(nonce_bytes);
 
@@ -31,10 +39,10 @@ pub fn decrypt_bytes(
 /** password in plain text, returns nonce_bytes and encrypted message */
 pub fn encrypt_bytes(bytes: Vec<u8>, password: String) -> Result<([u8; 12], Vec<u8>), String> {
     use aes_gcm_siv::aead::{Aead, NewAead};
-    use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
+    use aes_gcm_siv::{Aes256GcmSiv, Nonce}; // Or `Aes128GcmSiv`
 
-    let key = Key::from_slice(password.as_bytes());
-    let cipher = Aes256GcmSiv::new(key);
+    let key = generate_key(password)?;
+    let cipher = Aes256GcmSiv::new(&key);
 
     let nonce_bytes = generate_random_nonce();
 
@@ -60,19 +68,29 @@ pub fn hash_password(salt: String, password: String) -> String {
     hash
 }
 
-pub fn get_key_pair(path : &std::path::Path) -> Result<ring::signature::RsaKeyPair, CryptoError>  {
+pub fn get_key_pair(path: &std::path::Path) -> Result<ring::signature::RsaKeyPair, CryptoError> {
     let key_data = read_file(path)?;
-    Ok(ring::signature::RsaKeyPair::from_pkcs8(&key_data).map_err(|_| CryptoError::BadPrivateKey)?)
+    Ok(ring::signature::RsaKeyPair::from_pkcs8(&key_data)
+        .map_err(|_| CryptoError::BadPrivateKey)?)
 }
 
-pub fn sign_file(file_data: &Vec<u8>, file_name: &[u8], key_pair: &ring::signature::RsaKeyPair) -> Result<Vec<u8>, CryptoError> {
+pub fn sign_file(
+    file_data: &Vec<u8>,
+    file_name: &[u8],
+    key_pair: &ring::signature::RsaKeyPair,
+) -> Result<Vec<u8>, CryptoError> {
     let mut full_file = file_data.to_vec();
     full_file.append(&mut file_name.to_vec());
 
     let rng = ring::rand::SystemRandom::new();
     let mut signature = vec![0; key_pair.public_modulus_len()];
     key_pair
-        .sign(&ring::signature::RSA_PKCS1_SHA256, &rng, &full_file, &mut signature)
+        .sign(
+            &ring::signature::RSA_PKCS1_SHA256,
+            &rng,
+            &full_file,
+            &mut signature,
+        )
         .map_err(|_| CryptoError::OOM)?;
 
     Ok(signature)
